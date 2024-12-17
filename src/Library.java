436b5a3ffd9adb4423e51935336f2c8ea46cd403
kinanix7 <kinanix7279x@gmail.com>
2024-12-17 17:47:49 +0100

    Add Menu

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+    List<Book> books ;
+
+    public Library(){
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book){
+
+        books.add(book);
+        System.out.println("Susccessfully .");
+    }
+    public void displayBooks(){
+        for (Book book : books){
+            System.out.println(book);
+        }
+
+
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Library library = new Library();
+        Scanner scanner =new Scanner(System.in);
+
+        while(true) {
+             System.out.println("=========Menu=========\n");
+                System.out.println("1. Display Books");
+                System.out.println("2. Add Book");
+                System.out.println("3. Search Book");
+                System.out.println("4. Remove Book");
+                System.out.println("5. Edit Book");
+                System.out.println("6. Exit");
+
+                System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+               if (choice==6)
+               {
+                   break;
+               }
+
+                switch (choice){
+                    case 1 :library.displayBooks(); break;
+                    case 2 :
+                        break;
+                    case 3 : break;
+                    case 4 : break;
+                    case 5 : break;
+                    default:
+                        System.out.println("Invalid choice .");
+                        break;
+                }
+
+
+
+        }
+
     }
-}
+
+    }
